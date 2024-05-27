703ec3e591255216729e44f7fa8cd46a52c11ccf
Yashwanth39 <yaswanth.vanimina@sustainext.ai>
2024-05-27 18:04:42 +0530

    adding the Screen 2 changes for the energy screen

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env:{
-        BACKEND_API_URL : 'https://udm-be.sustainext.ai'
+        // BACKEND_API_URL : 'https://udm-be.sustainext.ai'
+        BACKEND_API_URL : 'http://localhost:8000'
     }
 };
 
