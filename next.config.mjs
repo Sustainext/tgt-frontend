97b9d9c78be9736ec2686e714b77a9edb9ab5e65
mahendras1ngh <iammahendrasingh@hotmail.com>
2024-05-31 17:19:06 +0530

    merged dev_integrated

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,9 @@
 const nextConfig = {
     env:{
         BACKEND_API_URL : 'https://udm-be.sustainext.ai',
-        NEXT_APP_TRACK_URL: 'https://app.powerbi.com/view?r=eyJrIjoiMDEzNmRjMjEtMjI4My00NzY4LWEyMGEtNDRiMjNkMmFkNjU0IiwidCI6ImIwZjhlODRjLWU0YTgtNDc5OS04MWIwLWRmMTUwMDY0MDM3ZCJ9'
+        NEXT_APP_TRACK_URL: 'https://app.powerbi.com/view?r=eyJrIjoiMDEzNmRjMjEtMjI4My00NzY4LWEyMGEtNDRiMjNkMmFkNjU0IiwidCI6ImIwZjhlODRjLWU0YTgtNDc5OS04MWIwLWRmMTUwMDY0MDM3ZCJ9',
+        NEXT_PUBLIC_APP_CLIMATIQ_KEY: 'HE3SK2V2F04MTSG9210JSQXB2QCX',
+        NEXT_PUBLIC_APP_CLIMATIQ_DATAVERSION: '8'
     }
 };
 
