78175cba6ff551de40dcc70e7f559e71ec4d7d85
Yashwanth39 <yaswanth.vanimina@sustainext.ai>
2024-05-31 17:57:03 +0530

    adding the env changes

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,7 @@
 const nextConfig = {
     env:{
         BACKEND_API_URL : 'https://udm-be.sustainext.ai',
+        // BACKEND_API_URL : 'http://127.0.0.1:8000',
         NEXT_APP_TRACK_URL: 'https://app.powerbi.com/view?r=eyJrIjoiMDEzNmRjMjEtMjI4My00NzY4LWEyMGEtNDRiMjNkMmFkNjU0IiwidCI6ImIwZjhlODRjLWU0YTgtNDc5OS04MWIwLWRmMTUwMDY0MDM3ZCJ9'
     }
 };
