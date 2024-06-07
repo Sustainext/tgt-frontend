57a176fefccd2668a624a73aca14c8e0a91c67ff
unknown <you@example.com>
2024-06-07 18:18:13 +0530

    emiission add

--- src/app/dashboard/environment/energy/energy-consumed/Purchased/purchased.jsx
+++ src/app/dashboard/environment/energy/energy-consumed/Purchased/purchased.jsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect,useRef } from 'react';
 import Form from '@rjsf/core';
 import validator from '@rjsf/validator-ajv8';
 import { MdAdd, MdOutlineDeleteOutline, MdInfoOutline } from "react-icons/md";
@@ -208,23 +208,149 @@ const generateTooltip = (field, title, tooltipText) => {
   );
 };
 
-const Purchased = () => {
+const Purchased = ({location, year, month}) => {
   const { open } = GlobalState();
   const [formData, setFormData] = useState([{}]);
   const [r_schema, setRemoteSchema] = useState({})
   const [r_ui_schema, setRemoteUiSchema] = useState({})
   const [loopen, setLoOpen] = useState(false);
-
+  const toastShown = useRef(false);
+  const getAuthToken = () => {
+      if (typeof window !== 'undefined') {
+          return localStorage.getItem('token')?.replace(/"/g, "");
+      }
+      return '';
+  };
+  const token = getAuthToken();
   const LoaderOpen = () => {
-    setLoOpen(true);
-  };
-  const LoaderClose = () => {
-    setLoOpen(false);
-  };
+      setLoOpen(true);
+    };
+    const LoaderClose = () => {
+      setLoOpen(false);
+    };
 
   const handleChange = (e) => {
-    setFormData(e.formData);
+      setFormData(e.formData); // Ensure you are extracting formData from the event
+  };
 
+  // The below code on updateFormData
+  let axiosConfig = {
+      headers: {
+        Authorization: 'Bearer ' + token,
+      },
+    };
+    const updateFormData = async () => {
+      LoaderOpen();
+      const data = {
+          client_id: client_id,
+          user_id: user_id,
+          path: view_path,
+          form_data: formData,
+          location,
+          year,
+          month
+      }
+
+      const url = `${process.env.BACKEND_API_URL}/datametric/update-fieldgroup`
+      try {
+          const response = await axios.post(url, data, axiosConfig);
+          if (response.status === 200) {
+              toast.success("Data added successfully", {
+                  position: "top-right",
+                  autoClose: 3000,
+                  hideProgressBar: false,
+                  closeOnClick: true,
+                  pauseOnHover: true,
+                  draggable: true,
+                  progress: undefined,
+                  theme: "light",
+              });
+              LoaderClose();
+              loadFormData();
+          } else {
+              toast.error("Oops, something went wrong", {
+                  position: "top-right",
+                  autoClose: 1000,
+                  hideProgressBar: false,
+                  closeOnClick: true,
+                  pauseOnHover: true,
+                  draggable: true,
+                  progress: undefined,
+                  theme: "colored",
+              });
+              LoaderClose();
+          }
+      } catch (error) {
+          toast.error("Oops, something went wrong", {
+              position: "top-right",
+              autoClose: 1000,
+              hideProgressBar: false,
+              closeOnClick: true,
+              pauseOnHover: true,
+              draggable: true,
+              progress: undefined,
+              theme: "colored",
+          });
+          LoaderClose();
+      }
+  };
+
+
+  const loadFormData = async () => {
+      LoaderOpen();
+      const url = `${process.env.BACKEND_API_URL}/datametric/get-fieldgroups?path_slug=${view_path}&client_id=${client_id}&user_id=${user_id}&location=${location}&year=${year}&month=${month}`;
+      try {
+          const response = await axios.get(url, axiosConfig);
+          console.log('API called successfully:', response.data);
+          setRemoteSchema(response.data.form[0].schema);
+          setRemoteUiSchema(response.data.form[0].ui_schema);
+          const form_parent = response.data.form_data;
+          setFormData(form_parent[0].data);
+      } catch (error) {
+          console.error('API call failed:', error);
+      } finally {
+          LoaderClose();
+      }
+  };
+  //Reloading the forms -- White Beard
+  useEffect(() => {
+      //console.long(r_schema, '- is the remote schema from django), r_ui_schema, '- is the remote ui schema from django')
+  },[r_schema, r_ui_schema])
+
+  // console log the form data change
+  useEffect(() => {
+      console.log('Form data is changed -', formData)
+  },[formData])
+
+  // fetch backend and replace initialized forms
+  useEffect(() => {
+      if (location && year && month) {
+          loadFormData();
+          toastShown.current = false; // Reset the flag when valid data is present
+      } else {
+          // Only show the toast if it has not been shown already
+          if (!toastShown.current) {
+              toast.warn("Please select location, year, and month first", {
+                  position: "top-right",
+                  autoClose: 5000,
+                  hideProgressBar: false,
+                  closeOnClick: true,
+                  pauseOnHover: true,
+                  draggable: true,
+                  progress: undefined,
+                  theme: "colored",
+              });
+              toastShown.current = true; // Set the flag to true after showing the toast
+          }
+      }
+  }, [location, year, month]); // Dependencies // React only triggers this effect if these dependencies change
+
+
+
+  const handleSubmit = (e) => {
+      e.preventDefault();
+      console.log('Form data:', formData);
+      updateFormData()
   };
 
   const handleAddNew = () => {
@@ -233,124 +359,7 @@ const Purchased = () => {
 
   };
   //The below code on updateFormData is by White Beard
-  const updateFormData = async () => {
-    const data = {
-      client_id: client_id,
-      user_id: user_id,
-      path: view_path,
-      form_data: formData
-    }
 
-    const url = `${process.env.BACKEND_API_URL}/datametric/update-fieldgroup`
-    try {
-      const response = await axios.post(url,
-        {
-          ...data
-        }
-      );
-
-      if (response.status === 200) {
-        toast.success("Data added successfully", {
-          position: "top-right",
-          autoClose: 3000,
-          hideProgressBar: false,
-          closeOnClick: true,
-          pauseOnHover: true,
-          draggable: true,
-          progress: undefined,
-          theme: "light",
-        });
-        LoaderClose();
-        loadFormData();
-
-      }else {
-        toast.error("Oops, something went wrong", {
-          position: "top-right",
-          autoClose: 1000,
-          hideProgressBar: false,
-          closeOnClick: true,
-          pauseOnHover: true,
-          draggable: true,
-          progress: undefined,
-          theme: "colored",
-        });
-        LoaderClose();
-      }
-    } catch (error) {
-      toast.error("Oops, something went wrong", {
-        position: "top-right",
-        autoClose: 1000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: "colored",
-      });
-      LoaderClose();
-    }
-  };
-
-  const loadFormData = async () => {
-    LoaderOpen();
-    const base_url = `${process.env.BACKEND_API_URL}/datametric/get-fieldgroups?path=`;
-    const url = `${base_url}${view_path}&&client_id=${client_id}&&user_id=${user_id}`
-    // console.log(url, 'is the url to be fired')
-
-    // making the GET request
-    axios.get(url)
-      .then(response => {
-        // handling the successful response
-        console.log(response.data, 'is the response data')
-        setRemoteSchema(response.data.form[0].schema)
-        setRemoteUiSchema(response.data.form[0].ui_schema)
-        const form_parent = response.data.form_data
-        const f_data = form_parent[0].data
-        setFormData(f_data)
-        LoaderClose();
-      })
-      .catch(error => {
-
-        const errorMessage =
-          error.response && error.response.data && error.response.data.message
-            ? error.response.data.message
-            : "Oops, something went wrong";
-        toast.error(errorMessage, {
-          position: "top-right",
-          autoClose: 5000,
-          hideProgressBar: false,
-          closeOnClick: true,
-          pauseOnHover: true,
-          draggable: true,
-          progress: undefined,
-          theme: "colored",
-        });
-        LoaderClose();
-      });
-  }
-
-  // Reloading the forms -- White Beard
-  useEffect(() => {
-    //console.long(r_schema, '- is the remote schema from django), r_ui_schema, '- is the remote ui schema from django')
-  }, [r_schema, r_ui_schema])
-
-  // console log the form data changes
-  useEffect(() => {
-    console.log('formdata is changed -', formData)
-  }, [formData])
-
-  // fetch backend and replace initialized forms
-  useEffect(() => {
-    console.log('From loaded , ready for trigger')
-    loadFormData()
-  }, [])
-
-  const handleSubmit = (e) => {
-    e.preventDefault(); // Prevent the default form submission
-    // console.log('Form data:', formData);
-    updateFormData()
-
-  };
   const updateFormDatanew = (updatedData) => {
     setFormData(updatedData);
 
