1435a2908d8b5055e9ddc7bff8c485bfb6fa0bd8
unknown <you@example.com>
2024-07-08 14:20:11 +0530

    i chnages

--- src/app/dashboard/environment/Water-effluents/water-stres/water-stres-body.jsx
+++ src/app/dashboard/environment/Water-effluents/water-stres/water-stres-body.jsx
@@ -29,9 +29,12 @@ const AccordionItem = ({ title, children, tooltiptext, sdg, display,location,set
         className="py-3 w-[100%] text-left flex"
         onClick={handleClick}// Unique ID for the tooltip, spaces replaced by dashes
       >
+        <div className="flex justify-between">
+        <div className={`flex ${open ? "w-[65vw]" : "w-[74vw]"}`}>
         <div className="flex items-center">
           <h5 className="text-[14px] text-[#344054] px-3">{title}</h5>
         </div>
+
 
         <div className="flex items-center justify-center relative">
           <MdInfoOutline
@@ -47,9 +50,11 @@ const AccordionItem = ({ title, children, tooltiptext, sdg, display,location,set
 
           </ReactTooltip>
         </div>
-        <div className={`absolute flex justify-between ${isOpen ? 'right-[3rem]' : 'right-[3rem]'}`}>
+        </div>
+       <div className=" w-[20vw] ">
+       <div className={`flex float-end`}>
 
-            <>
+              <>
               {sdg && sdg.map((sdgItem, index) => (
                 <div key={index} className="bg-sky-100 h-[25px] w-[70px] rounded-md mx-2">
                   <p className="text-[#0057A5] text-[10px] inline-block align-middle px-2 font-semibold">{sdgItem}</p>
@@ -57,7 +62,10 @@ const AccordionItem = ({ title, children, tooltiptext, sdg, display,location,set
               ))}
             </>
 
+
           <MdKeyboardArrowDown className={`text-2xl ${isOpen ? "rotate-180" : ""}`} />
+        </div>
+       </div>
         </div>
       </button>
       {isOpen && <div className="p-4">{children}</div>}
