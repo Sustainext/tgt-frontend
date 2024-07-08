85945c2dcd15cb92f31cb3d16c2dfe71af2cec52
unknown <you@example.com>
2024-07-08 19:42:37 +0530

    refrsh add

--- src/app/dashboard/social/Human-Rights/child-labour/screen3.jsx
+++ src/app/dashboard/social/Human-Rights/child-labour/screen3.jsx
@@ -49,12 +49,15 @@ const uiSchema = {
     },
 };
 const Screen3 = ({ location, year, month }) => {
-    const [formData, setFormData] = useState([{
-        childlabor: "",
-        TypeofOperation: "",
-        geographicareas: "",
+    const initialFormData = [
+        {
+            childlabor: "",
+            TypeofOperation: "",
+            geographicareas: "",
 
-    }]);
+        },
+    ];
+    const [formData, setFormData] = useState(initialFormData);
     const [r_schema, setRemoteSchema] = useState({})
     const [r_ui_schema, setRemoteUiSchema] = useState({})
     const [loopen, setLoOpen] = useState(false);
@@ -149,6 +152,7 @@ const Screen3 = ({ location, year, month }) => {
 
     const loadFormData = async () => {
         LoaderOpen();
+        setFormData(initialFormData);
         const url = `${process.env.BACKEND_API_URL}/datametric/get-fieldgroups?path_slug=${view_path}&client_id=${client_id}&user_id=${user_id}&location=${location}&year=${year}&month=${month}`;
 
         try {
@@ -156,14 +160,13 @@ const Screen3 = ({ location, year, month }) => {
             console.log('API called successfully:', response.data);
             setRemoteSchema(response.data.form[0].schema);
             setRemoteUiSchema(response.data.form[0].ui_schema);
-            const form_parent = response.data.form_data;
-            setFormData(form_parent[0].data);
-            const f_data = form_parent[0].data
-            setFormData(f_data)
+            setFormData(response.data.form_data[0].data);
         } catch (error) {
             console.error('API call failed:', error);
+            setFormData(initialFormData);
         } finally {
             LoaderClose();
+
         }
     }
     //Reloading the forms
