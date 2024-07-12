0c3c87700a77f189d7b2de68ef9069216c81e307
mahendras1ngh <iammahendrasingh@hotmail.com>
2024-07-12 16:14:29 +0530

    Deployment 12/07

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 const nextConfig = {
     env: {
-        BACKEND_API_URL: 'https://udm-be-dev.sustainext.ai',
+        BACKEND_API_URL: 'https://udm-be.sustainext.ai',
         // BACKEND_API_URL: 'http://127.0.0.1:8000',
         NEXT_APP_TRACK_URL: 'https://analytics.zoho.in/open-view/283231000002209106',
         NEXT_PUBLIC_APP_CLIMATIQ_KEY: 'HE3SK2V2F04MTSG9210JSQXB2QCX',
