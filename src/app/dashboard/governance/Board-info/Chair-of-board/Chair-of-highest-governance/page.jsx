42aee8fb66f54f6798f403403e782e4dd3107714
mahendras1ngh <iammahendrasingh@hotmail.com>
2024-08-08 11:40:56 +0530

    api integration

--- src/app/dashboard/governance/Board-info/Chair-of-board/Chair-of-highest-governance/page.jsx
+++ src/app/dashboard/governance/Board-info/Chair-of-board/Chair-of-highest-governance/page.jsx
@@ -18,7 +18,7 @@ const widgets = {
   TableWidget: CustomTableWidget,
 };
 
-const view_path = "gri-social-product_labeling-417-1a-required";
+const view_path = "gri-governance-chair_of_board-2-11-b-chair";
 const client_id = 1;
 const user_id = 1;
 
@@ -217,7 +217,7 @@ const ChairOfHighestGovernance = ({ selectedOrg, year, selectedCorp }) => {
   const loadFormData = async () => {
     LoaderOpen();
     setFormData([{}]);
-    const url = `${process.env.BACKEND_API_URL}/datametric/get-fieldgroups?path_slug=${view_path}&client_id=${client_id}&user_id=${user_id}&location=${location}&year=${year}`;
+    const url = `${process.env.BACKEND_API_URL}/datametric/get-fieldgroups?path_slug=${view_path}&client_id=${client_id}&user_id=${user_id}&corporate=${selectedCorp}&organisation=${selectedOrg}&year=${year}`;
     try {
       const response = await axiosInstance.get(url);
       console.log("API called successfully:", response.data);
@@ -244,7 +244,7 @@ const ChairOfHighestGovernance = ({ selectedOrg, year, selectedCorp }) => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // updateFormData();
+    updateFormData();
     console.log("test form data", formData);
   };
 
@@ -284,8 +284,8 @@ const ChairOfHighestGovernance = ({ selectedOrg, year, selectedCorp }) => {
         <div className="mx-2 flex">
           <div className="w-[92%] relative">
             <Form
-              schema={schema}
-              uiSchema={uiSchema}
+              schema={r_schema}
+              uiSchema={r_ui_schema}
               formData={formData}
               onChange={handleChange}
               validator={validator}
--- src/app/dashboard/governance/Board-info/Nomination-and-selection/Process/page.jsx
+++ src/app/dashboard/governance/Board-info/Nomination-and-selection/Process/page.jsx
@@ -9,12 +9,13 @@ import "react-tooltip/dist/react-tooltip.css";
 import axios from "axios";
 import { toast } from "react-toastify";
 import { Oval } from "react-loader-spinner";
+import axiosInstance from '@/app/utils/axiosMiddleware'
 
 const widgets = {
   inputWidget: InputWidget5,
 };
 
-const view_path = "";
+const view_path = "gri-governance-nomination-2-10-a-nomination";
 const client_id = 1;
 const user_id = 1;
 
@@ -59,28 +60,17 @@ const Process = ({ selectedOrg, selectedCorp, year, month }) => {
   const [r_ui_schema, setRemoteUiSchema] = useState({});
   const [loopen, setLoOpen] = useState(false);
   const toastShown = useRef(false);
-  const getAuthToken = () => {
-    if (typeof window !== "undefined") {
-      return localStorage.getItem("token")?.replace(/"/g, "");
-    }
-    return "";
-  };
-  const token = getAuthToken();
+
   const LoaderOpen = () => {
     setLoOpen(true);
   };
+
   const LoaderClose = () => {
     setLoOpen(false);
   };
 
   const handleChange = (e) => {
     setFormData(e.formData);
-  };
-
-  let axiosConfig = {
-    headers: {
-      Authorization: "Bearer " + token,
-    },
   };
 
   const updateFormData = async () => {
@@ -97,7 +87,7 @@ const Process = ({ selectedOrg, selectedCorp, year, month }) => {
 
     const url = `${process.env.BACKEND_API_URL}/datametric/update-fieldgroup`;
     try {
-      const response = await axios.post(url, data, axiosConfig);
+      const response = await axiosInstance.post(url, data);
       if (response.status === 200) {
         toast.success("Data added successfully", {
           position: "top-right",
@@ -142,9 +132,10 @@ const Process = ({ selectedOrg, selectedCorp, year, month }) => {
   const loadFormData = async () => {
     LoaderOpen();
     setFormData([{}]);
-    const url = `${process.env.BACKEND_API_URL}/datametric/get-fieldgroups?path_slug=${view_path}&client_id=${client_id}&user_id=${user_id}&selectedOrg=${selectedOrg}&year=${year}&month=${month}`;
+    const view_path = 'gri-governance-nomination-2-10-a-nomination';
+    const url = `${process.env.BACKEND_API_URL}/datametric/get-fieldgroups?path_slug=${view_path}&client_id=${client_id}&user_id=${user_id}&organisation=${selectedOrg}&corporate=${selectedCorp}&year=${year}`;
     try {
-      const response = await axios.get(url, axiosConfig);
+      const response = await axiosInstance.get(url);
       console.log("API called successfully:", response.data);
       setRemoteSchema(response.data.form[0].schema);
       setRemoteUiSchema(response.data.form[0].ui_schema);
@@ -170,7 +161,7 @@ const Process = ({ selectedOrg, selectedCorp, year, month }) => {
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log("Form data:", formData);
-    // updateFormData();
+    updateFormData();
   };
 
   return (
@@ -219,8 +210,8 @@ const Process = ({ selectedOrg, selectedCorp, year, month }) => {
         </div>
         <div className="mx-2">
           <Form
-            schema={schema}
-            uiSchema={uiSchema}
+            schema={r_schema}
+            uiSchema={r_ui_schema}
             formData={formData}
             onChange={handleChange}
             validator={validator}
