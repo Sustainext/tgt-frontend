df4d3302e57e38f7f6d54210b73f94a299a5d409
himanshu-next <himanshu.banswal@sustainext.ai>
2024-08-21 16:22:39 +0530

    changed the materilaity select material topic step 3 condition

--- src/app/dashboard/Materiality/accordance/selectMaterialityTopic/steps/step3.jsx
+++ src/app/dashboard/Materiality/accordance/selectMaterialityTopic/steps/step3.jsx
@@ -25,60 +25,95 @@ const user_id = 1;
 const schema = {
     type: "array",
     items: {
-        type: "object",
-        properties: {
-            Q1: {
-                type: "string",
-                title: "Are there any changes to the list of material topics compared to the previous reporting period?",
-                enum: ["Yes", "No"],
-
-            },
-            Q2: {
-                type: "string",
-                title: "If yes, specify the changes to the list of material topics compared to the previous reporting period?",
-
-            },
-        },
-
-    },
-};
-
-const uiSchema = {
-    items: {
-        "ui:order": ["Q1", "Q2"],
+      type: "object",
+      properties: {
         Q1: {
-            "ui:title": "Are there any changes to the list of material topics compared to the previous reporting period?",
-            "ui:tooltip":
-                "<p>Indicate whether any changes are there to the list of material topics compared to the previous reporting period.</p> <p>Reporting period: </p> <p>Specific time period covered by the reported information.</p>",
-            "ui:tooltipdisplay": "block",
-            "ui:widget": "MaterialityRadioWidget",
-            "ui:tag":"GRI-3-2-b",
-            "ui:horizontal": true,
-            "ui:options": {
-                label: false,
+          type: "string",
+          title: "Are there any changes to the list of material topics compared to the previous reporting period?",
+          enum: ["Yes", "No"],
+        },
+      },
+      dependencies: {
+        Q1: {
+          oneOf: [
+            {
+              properties: {
+                Q1: {
+                  enum: ["Yes"],
+                },
+                Q2: {
+                  type: "string",
+                  title: "If yes, specify the changes to the list of material topics compared to the previous reporting period?",
+                },
+  
+              },
             },
+          ],
         },
-        Q2: {
-            "ui:title": "If yes, specify the changes to the list of material topics compared to the previous reporting period?",
-            "ui:tooltip":
-                "<p>Here organization can explain why a topic that it determined as material in the previous reporting period is no longer considered to be material or why a new topic has been determined as material for the current reporting period.</p> <p>Reporting period: </p> <p>Specific time period covered by the reported information.</p>",
-            "ui:tooltipdisplay": "block",
-            "ui:widget": "MaterialityInputWidget",
-            "ui:tag":"GRI-3-2-b",
-            "ui:horizontal": true,
-            "ui:options": {
-                label: false,
-            },
-        },
-
-        "ui:options": {
-            orderable: false, // Prevent reordering of items
-            addable: false, // Prevent adding items from UI
-            removable: false, // Prevent removing items from UI
-            layout: "horizontal", // Set layout to horizontal
-        },
+      },
     },
-};
+  };
+  
+  const uiSchema = {
+    items: {
+      "ui:order": ["Q1", "Q2"],
+      Q1: {
+        "ui:title": "Are there any changes to the list of material topics compared to the previous reporting period?",
+        "ui:tooltip":
+        "<p>Indicate whether any changes are there to the list of material topics compared to the previous reporting period.</p> <p>Reporting period: </p> <p>Specific time period covered by the reported information.</p>",
+        "ui:tooltipdisplay": "block",
+        "ui:widget": "MaterialityRadioWidget",
+        "ui:tag":"GRI-3-2-b",
+        "ui:horizontal": true,
+        "ui:options": {
+          label: false,
+        },
+      },
+      Q2: {
+        "ui:title":
+          "If yes, specify the changes to the list of material topics compared to the previous reporting period?",
+          "ui:tooltip":
+          "<p>Here organization can explain why a topic that it determined as material in the previous reporting period is no longer considered to be material or why a new topic has been determined as material for the current reporting period.</p> <p>Reporting period: </p> <p>Specific time period covered by the reported information.</p>",
+        "ui:tooltipdisplay": "block",
+        "ui:widget": "MaterialityInputWidget",
+        "ui:tag":"GRI-3-2-b",
+        "ui:horizontal": true,
+        "ui:options": {
+          label: false,
+        },
+      },
+      Q3: {
+          "ui:title":
+            "Provide the total number of instances for which fines were incurred",
+          "ui:tooltip":
+            " Please specify what has been assured and on what basis it has been assured.",
+          "ui:tooltipdisplay": "none",
+          "ui:widget": "inputWidget",
+          "ui:horizontal": true,
+          "ui:options": {
+            label: false,
+          },
+        },
+        Q4: {
+          "ui:title":
+            "Provide the total number of instances for which non-monetary sanctions were incurred",
+          "ui:tooltip":
+            "Please specify the standard used for assurance. ",
+          "ui:tooltipdisplay": "none",
+          "ui:widget": "inputWidget",
+          "ui:horizontal": true,
+          "ui:options": {
+            label: false,
+          },
+        },
+      "ui:options": {
+        orderable: false, // Prevent reordering of items
+        addable: false, // Prevent adding items from UI
+        removable: false, // Prevent removing items from UI
+        layout: "horizontal", // Set layout to horizontal
+      },
+    },
+  };
 
 const Step3 = ({ selectedOrg, year, selectedCorp }) => {
     const [formData, setFormData] = useState([{}]);
