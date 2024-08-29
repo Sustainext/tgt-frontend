139fdef5e3487815ed0db142eecf7c40fa954793
himanshu-next <himanshu.banswal@sustainext.ai>
2024-08-29 12:12:40 +0530

    added economic sidebar

--- src/app/dashboard/sidebar.jsx
+++ src/app/dashboard/sidebar.jsx
@@ -61,7 +61,7 @@ const Sidenav = () => {
         {
           title: "Economic",
           icon: <MdOutlineDiversity3 />,
-          link: "/dashboard/environment",
+          link: "/dashboard/Economic",
         },
       ],
     },
