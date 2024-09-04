d7581c87aa6721a921d531e2ced1c6074aff84a2
Utsavsustainx <utsav.pipersaniya@sustainext.ai>
2024-09-04 17:09:32 +0530

    fixed import

--- src/app/dashboard/economic/economic-performance/defined-benefit/page.jsx
+++ src/app/dashboard/economic/economic-performance/defined-benefit/page.jsx
@@ -11,7 +11,7 @@ import Screen1 from "./screen1";
 import Screen2 from "./screen2";
 import Screen3 from "./screen3";
 import Screen4 from "./screen4";
-import Screen5 from "./screen5";
+import Screen5 from "./Screen5";
 const Definedbenefit = () => {
   const [activeMonth, setActiveMonth] = useState("");
   const [selectedLocation, setSelectedLocation] = useState("");
