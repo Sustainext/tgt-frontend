3c69029643451ad0a1813e2965227cde5ee6fae3
shubham <152493900+shubham-sustainext@users.noreply.github.com>
2024-09-12 15:40:31 +0530

    test lg

--- src/app/dashboard/layout.js
+++ src/app/dashboard/layout.js
@@ -6,26 +6,26 @@ import { GlobalState } from '../../Context/page';
 
 export default function DashboardLayout({ children }) {
   const { open } = GlobalState();
-  // const [defaultLanguage, setDefaultLanguage] = useState('ja'); 
+  const [defaultLanguage, setDefaultLanguage] = useState('ja'); 
 
 
 
-  // useEffect(() => {
-  //   if (!document.getElementById('gtranslate-script')) {
-  //     window.gtranslateSettings = {
-  //       default_language: 'en',
-  //       native_language_names: true,
-  //       languages: ['en', 'fr', 'ja', 'hi'],
-  //       wrapper_selector: '.gtranslate_wrapper',
-  //     };
+  useEffect(() => {
+    if (!document.getElementById('gtranslate-script')) {
+      window.gtranslateSettings = {
+        default_language: 'en',
+        native_language_names: true,
+        languages: ['en', 'fr', 'ja', 'hi'],
+        wrapper_selector: '.gtranslate_wrapper',
+      };
 
-  //     const script = document.createElement('script');
-  //     script.src = 'https://cdn.gtranslate.net/widgets/latest/dropdown.js';
-  //     script.defer = true;
-  //     script.id = 'gtranslate-script';
-  //     document.body.appendChild(script);
-  //   }
-  // }, [defaultLanguage]); 
+      const script = document.createElement('script');
+      script.src = 'https://cdn.gtranslate.net/widgets/latest/dropdown.js';
+      script.defer = true;
+      script.id = 'gtranslate-script';
+      document.body.appendChild(script);
+    }
+  }, [defaultLanguage]); 
 
   return (
     <>
@@ -37,7 +37,7 @@ export default function DashboardLayout({ children }) {
           <div className={`mx-2 w-full ${open ? 'ml-[243px]' : 'ml-[87px]'}`}>
             <div className="mb-5">
               <DashboardHeader />
-              {/* <div className="gtranslate_wrapper" style={{ margin: '2px 0' }}></div> */}
+              <div className="gtranslate_wrapper" style={{ margin: '2px 0' }}></div>
               <div>{children}</div>
             </div>
           </div>
