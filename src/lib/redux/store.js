0de084c93bd39e935227dd8e451557f33f5f75d7
mahendras1ngh <iammahendrasingh@hotmail.com>
2024-09-25 10:13:27 +0530

    config and store

--- src/lib/redux/store.js
+++ src/lib/redux/store.js
@@ -3,10 +3,12 @@ import { combineReducers } from 'redux'
 
 // Import reducers
 import counterReducer from './features/counterSlice'
+import emissionReducer from './features/emissionSlice'
 // Import other reducers as needed
 
 const rootReducer = combineReducers({
   counter: counterReducer,
+  emissions: emissionReducer,
   // Add other reducers here
 })
 
