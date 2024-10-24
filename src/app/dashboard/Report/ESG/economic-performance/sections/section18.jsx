6e0449fb25cded03b5cddce177f73760029dc959
himanshu-next <himanshu.banswal@sustainext.ai>
2024-10-24 16:33:39 +0530

    esg report

--- src/app/dashboard/Report/ESG/economic-performance/sections/section18.jsx
+++ src/app/dashboard/Report/ESG/economic-performance/sections/section18.jsx
@@ -48,21 +48,6 @@ const Section18=({section11_5_3Ref})=>{
         )
         }
 
-
-{data["205_3d_anti_corruption"]?.length>0?data["205_3d_anti_corruption"].map((item, index) => (
-          <div key={`financial_implications_${index}`} className="mb-4">
-            <p className="text-sm mb-2">{item.Q1 || "No data available"}</p>
-            
-            {/* Only show Q2 if Q1 is not 'Yes' and Q2 has a value */}
-            {item.Q1 == "Yes" && item.Q2 && <p className="text-sm mb-2">{item.Q2}</p>}
-
-            <p className="text-sm mb-2">{item.Q3 || "No data available"}</p>
-          </div>
-        ))
-        :(
-          <p className="text-sm mb-2">No data available</p>
-        )
-      }
         </div>
         </>
     )
