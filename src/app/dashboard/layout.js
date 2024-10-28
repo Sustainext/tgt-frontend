07badd79de0f86cb0f7e70f0d6f814e378bfd01c
shubham <152493900+shubham-sustainext@users.noreply.github.com>
2024-10-28 22:29:33 +0530

    roele parmission add org strucher

--- src/app/dashboard/layout.js
+++ src/app/dashboard/layout.js
@@ -1,17 +1,24 @@
 'use client';
 import { useState, useEffect } from 'react';
+import { useRouter } from 'next/navigation'; // Import useRouter for navigation
 import DashboardHeader from './dashobardheader';
 import Sidenav from './sidebar';
 import { GlobalState } from '../../Context/page';
 import StoreProvider from '../../Context/storeProvider';
 import GlobalErrorHandler from '../shared/components/GlobalErrorHandler';
 
-
 export default function DashboardLayout({ children }) {
   const { open } = GlobalState();
-  const [defaultLanguage, setDefaultLanguage] = useState('ja'); 
+  const [defaultLanguage, setDefaultLanguage] = useState('ja');
+  const router = useRouter(); // Initialize router
 
-
+  useEffect(() => {
+    // Redirect if token is not set in local storage
+    const token = localStorage.getItem('token');
+    if (!token) {
+      router.push('/'); // Redirect to home page if no token
+    }
+  }, [router]); // Include router in the dependency array
 
   useEffect(() => {
     if (!document.getElementById('gtranslate-script')) {
@@ -28,28 +35,26 @@ export default function DashboardLayout({ children }) {
       script.id = 'gtranslate-script';
       document.body.appendChild(script);
     }
-  }, [defaultLanguage]); 
+  }, [defaultLanguage]);
 
   return (
     <>
-
-        <section>
-          <GlobalErrorHandler />
-          <StoreProvider>
-            <div className="flex w-full">
-              <div className="block float-left">
-                <Sidenav />
-              </div>
-              <div className={`mx-2 w-full ${open ? 'ml-[243px]' : 'ml-[74px]'}`}>
-                <div className="mb-5">
-                  <DashboardHeader />
-                  <div>{children}</div>
-                </div>
+      <section>
+        <GlobalErrorHandler />
+        <StoreProvider>
+          <div className="flex w-full">
+            <div className="block float-left">
+              <Sidenav />
+            </div>
+            <div className={`mx-2 w-full ${open ? 'ml-[243px]' : 'ml-[74px]'}`}>
+              <div className="mb-5">
+                <DashboardHeader />
+                <div>{children}</div>
               </div>
             </div>
-          </StoreProvider>
-        </section>
- 
+          </div>
+        </StoreProvider>
+      </section>
     </>
   );
 }
