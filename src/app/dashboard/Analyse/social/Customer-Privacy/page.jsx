872a064b50d0a501884880e997589b335e999393
himanshu-next <himanshu.banswal@sustainext.ai>
2024-11-06 15:30:17 +0530

    analyse changes

--- src/app/dashboard/Analyse/social/Customer-Privacy/page.jsx
+++ src/app/dashboard/Analyse/social/Customer-Privacy/page.jsx
@@ -1,344 +1,38 @@
 "use client";
 import React, { useState, useEffect } from "react";
-import TableSidebar from "./TableSidebar";
-import DynamicTable2 from "./customTable2";
-import DateRangePicker from "../../../../utils/DatePickerComponent";
 import axiosInstance from "../../../../utils/axiosMiddleware";
-import { columns1 } from "./data";
-import { Oval } from "react-loader-spinner";
+import AnalyseHeader5 from "../../AnalyseHeader5";
+import Section from "./section";
+
 const AnalyseCustomerprivacy = ({ isBoxOpen }) => {
-  const [organisations, setOrganisations] = useState([]);
+
   const [selectedOrg, setSelectedOrg] = useState("");
   const [selectedCorp, setSelectedCorp] = useState("");
-  const [customerhealth, setCustomerhealth] = useState([]);
-  const [corporates, setCorporates] = useState([]);
-  const [reportType, setReportType] = useState("Organization");
-  const [loopen, setLoOpen] = useState(false);
   const [dateRange, setDateRange] = useState({
     start: null,
     end: null,
   });
 
-  const [isDateRangeValid, setIsDateRangeValid] = useState(true);
-  const [datasetparams, setDatasetparams] = useState({
-    organisation: "",
-    corporate: "",
-    start: null,
-    end: null,
-  });
-  const LoaderOpen = () => {
-    setLoOpen(true);
-  };
-  const LoaderClose = () => {
-    setLoOpen(false);
-  };
-  const fetchData = async (params) => {
-    if (!params.start || !params.end) {
-      setIsDateRangeValid(false);
-      console.error("Invalid date range selected");
-      return;
-    } else {
-      const startDate = new Date(params.start);
-      const endDate = new Date(params.end);
 
-      if (endDate < startDate) {
-        setIsDateRangeValid(false);
-        setDateRange({
-          start: null,
-          end: null,
-        });
-        console.error("End date cannot be before start date");
-        return;
-      } else {
-        setIsDateRangeValid(true);
-      }
-    }
-
-    LoaderOpen();
-    try {
-      const response = await axiosInstance.get(
-        `/sustainapp/get_customer_privacy_analysis`,
-        {
-          params: params,
-        }
-      );
-
-      const data = response.data;
-
-      const { customer_privacy_data } = data;
-
-      const formatcustomerhealth = (data) => {
-        console.log(data, "test loacl data");
-        return data.map((data, index) => ({
-          "Number of substantiated complaints received concerning breaches of customer privacy":
-            data.customerprivacy,
-          "Complaints received from outside parties and substantiated by the organization":
-            data.substantiatedorganization,
-          "Complaints from regulatory bodies": data.regulatorybodies,
-        }));
-      };
-      setCustomerhealth(formatcustomerhealth(customer_privacy_data));
-      LoaderClose();
-    } catch (error) {
-      console.error("There was a problem with the fetch operation:", error);
-      LoaderClose();
-    }
-  };
-  useEffect(() => {
-    fetchData(datasetparams);
-  }, [datasetparams]);
-
-  useEffect(() => {
-    const fetchOrg = async () => {
-      try {
-        const response = await axiosInstance.get(`/orggetonly`);
-        setOrganisations(response.data);
-      } catch (e) {
-        console.error("Failed fetching organization:", e);
-      }
-    };
-
-    fetchOrg();
-  }, []);
-
-  useEffect(() => {
-     const fetchCorporates = async () => {
-      if (selectedOrg) {
-        try {
-          const response = await axiosInstance.get(`/corporate/`, {
-            params: { organization_id: selectedOrg },
-          });
-          setCorporates(response.data);
-        } catch (e) {
-          if(e.status === 404) {
-            setCorporates([]);
-          }
-          else{
-            console.error("Failed fetching corporates:", e);
-          }
-          
-        }
-      }
-    };
-
-    fetchCorporates();
-  }, [selectedOrg]);
-
-  const handleReportTypeChange = (type) => {
-    setReportType(type);
-  };
-
-  const handleOrganizationChange = (e) => {
-    const newOrg = e.target.value;
-    setSelectedOrg(newOrg);
-    setSelectedCorp("");
-    setDateRange({
-      start: null,
-      end: null,
-    });
-    setDatasetparams(() => ({
-      start: null,
-      end: null,
-      organisation: newOrg,
-      corporate: "",
-    }));
-  };
-
-  const handleOrgChange = (e) => {
-    const newCorp = e.target.value;
-    setSelectedCorp(newCorp);
-    setDateRange({
-      start: null,
-      end: null,
-    });
-    setDatasetparams(() => ({
-      start: null,
-      end: null,
-      corporate: newCorp,
-      organisation: selectedOrg,
-    }));
-  };
-
-  const handleDateChange = (newRange) => {
-    setDateRange(newRange);
-
-    setDatasetparams((prevParams) => ({
-      ...prevParams,
-      start: newRange.start,
-      end: newRange.end,
-    }));
-  };
 
   return (
-    <div>
-      <div>
-        <div className="mb-2 flex-col items-center pt-4  gap-6">
-          <div className="mt-4 pb-3 mx-5 text-left">
-            <div className="mb-2 flex-col items-center pt-2  gap-6">
-              <div className="justify-start items-center gap-4 inline-flex">
-                <div className="text-zinc-600 text-[12px] font-semibold font-['Manrope']">
-                  View By:
-                </div>
-                <div className="rounded-lg shadow border border-gray-300 justify-start items-start flex">
-                  <div
-                    className={`w-[111px] px-4 py-2.5 border-r rounded-l-lg border-gray-300 justify-center items-center gap-2 flex cursor-pointer ${
-                      reportType === "Organization" ? "bg-sky-100" : "bg-white"
-                    }`}
-                    onClick={() => handleReportTypeChange("Organization")}
-                  >
-                    <div className="text-slate-800 text-[12px] font-medium font-['Manrope'] leading-tight">
-                      Organization
-                    </div>
-                  </div>
-                  <div
-                    className={`w-[111px] px-4 py-2.5 border-r border-gray-300 justify-center items-center gap-2 flex cursor-pointer ${
-                      reportType === "Corporate" ? "bg-sky-100" : "bg-white"
-                    }`}
-                    onClick={() => handleReportTypeChange("Corporate")}
-                  >
-                    <div className="text-slate-700 text-[12px] font-medium font-['Manrope'] leading-tight">
-                      Corporate
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div
-                className={`grid grid-cols-1 md:grid-cols-4 w-[80%] mb-2 pt-4 ${
-                  reportType !== "" ? "visible" : "hidden"
-                }`}
-              >
-                <div className="mr-2">
-                  <label
-                    htmlFor="cname"
-                    className="text-neutral-800 text-[12px] font-normal ml-1"
-                  >
-                    Select Organization*
-                  </label>
-                  <div className="mt-2">
-                    <select
-                      className="block w-full rounded-md border-0 py-1.5 pl-4 text-neutral-500 text-[12px] font-normal leading-tight ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600"
-                      value={selectedOrg}
-                      onChange={handleOrganizationChange}
-                    >
-                      <option value="01">--Select Organization--- </option>
-                      {organisations &&
-                        organisations.map((org) => (
-                          <option key={org.id} value={org.id}>
-                            {org.name}
-                          </option>
-                        ))}
-                    </select>
-                  </div>
-                </div>
-                {(reportType === "Corporate" || reportType === "Location") && (
-                  <div className="mr-2">
-                    <label
-                      htmlFor="cname"
-                      className="text-neutral-800 text-[12px] font-normal ml-1"
-                    >
-                      Select Corporate
-                    </label>
-                    <div className="mt-2">
-                      <select
-                        className="block w-full rounded-md border-0 py-1.5 pl-4 text-neutral-500 text-[12px] font-normal leading-tight ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600"
-                        value={selectedCorp}
-                        onChange={handleOrgChange}
-                      >
-                        <option value="">--Select Corporate--- </option>
-                        {corporates &&
-                          corporates.map((corp) => (
-                            <option key={corp.id} value={corp.id}>
-                              {corp.name}
-                            </option>
-                          ))}
-                      </select>
-                    </div>
-                  </div>
-                )}
-
-                <div className="mr-2">
-                  <label
-                    htmlFor="cname"
-                    className="text-neutral-800 text-[12px] font-normal ml-1"
-                  >
-                    Select Date
-                  </label>
-                  <div className="mt-2">
-                    <DateRangePicker
-                      startDate={dateRange.start}
-                      endDate={dateRange.end}
-                      onDateChange={handleDateChange}
-                    />
-                    {!isDateRangeValid && (
-                      <div className="text-red-600 text-[12px]">
-                        Please select a valid date range.
-                      </div>
-                    )}
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="flex">
-          <div className={`ps-4 w-[100%] me-4`}>
-            <div className="mb-6">
-              <div
-                id="ep1"
-                className="text-neutral-700 text-[15px] font-normal font-['Manrope'] leading-tight mb-3 "
-              >
-                <div>
-                  <p className="text-[15px] font-bold">
-                    Substantiated complaints concerning breaches of customer
-                    privacy and losses of customer data
-                  </p>
-                </div>
-                <div className="flex justify-between items-center mb-2">
-                  <p className="text-gray-500 text-[13px]">
-                    Total number of substantiated complaints received concerning
-                    breaches of customer privacy
-                  </p>
-
-                  <div className="w-[70px] h-[26px] p-2 bg-sky-700 bg-opacity-5 rounded-lg justify-center items-center gap-2 inline-flex">
-                    <div className="text-sky-700 text-[10px] font-semibold font-['Manrope'] leading-[10px] tracking-tight">
-                      GRI 418-1a
-                    </div>
-                  </div>
-                </div>
-                <div className="mb-4">
-                  <DynamicTable2 columns={columns1} data={customerhealth} />
-                </div>
-              </div>
-            </div>
-          </div>
-          <div
-            style={{
-              position: `${isBoxOpen ? "unset" : "sticky"}`,
-              top: "10rem",
-              // zIndex: "0",
-              height: "fit-content",
-              backgroundColor: "white",
-              paddingBottom: "1rem",
-            }}
-            className=" mb-8 me-2"
-          >
-            <TableSidebar />
-          </div>
-        </div>
-        {loopen && (
-          <div className=" fixed inset-0 flex items-center justify-center z-[100] bg-black bg-opacity-50">
-            <Oval
-              height={50}
-              width={50}
-              color="#00BFFF"
-              secondaryColor="#f3f3f3"
-              strokeWidth={2}
-              strokeWidthSecondary={2}
-            />
-          </div>
-        )}
-      </div>
-    </div>
+    <>
+   
+        <AnalyseHeader5
+          selectedOrg={selectedOrg}
+          setSelectedOrg={setSelectedOrg}
+          selectedCorp={selectedCorp}
+          setSelectedCorp={setSelectedCorp}
+          dateRange={dateRange}
+          setDateRange={setDateRange}
+        />
+        <Section
+          selectedOrg={selectedOrg}
+          selectedCorp={selectedCorp}
+          dateRange={dateRange}
+          isBoxOpen={isBoxOpen}
+        />
+      </>
   );
 };
 
