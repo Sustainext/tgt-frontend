087562decfebbbe51865c81a162b85288161777c
mahendras1ngh <iammahendrasingh@hotmail.com>
2024-12-05 16:46:58 +0530

    enabled emission validation

--- src/app/dashboard/environment/Emissions/scope3new.jsx
+++ src/app/dashboard/environment/Emissions/scope3new.jsx
@@ -533,12 +533,12 @@ const Scope3 = forwardRef(
           >
             + Add new
           </button>
-          {/* {showError && (
+          {showError && (
             <div className="text-xs text-red-500 mt-4 flex items-center">
               <MdError />
               <span>{dataError}</span>
             </div>
-          )} */}
+          )}
         </div>
         {loopen && (
           <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50">
