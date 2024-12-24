c23b5da69b5a512d85be82ecd6881d177ed816d5
mahendras1ngh <iammahendrasingh@hotmail.com>
2024-12-24 17:26:15 +0530

    translator

--- src/app/dashboard/layout.js
+++ src/app/dashboard/layout.js
@@ -1,16 +1,18 @@
-'use client';
-import { useState, useEffect } from 'react';
-import { useRouter } from 'next/navigation'; // Import useRouter for navigation
-import DashboardHeader from './dashobardheader';
-import Sidenav from './sidebar';
-import { GlobalState } from '../../Context/page';
-import StoreProvider from '../../Context/storeProvider';
-import GlobalErrorHandler from '../shared/components/GlobalErrorHandler';
-import AutoTranslator from "./gt"
-import TranslationPanel from "./testtrnslator"
+"use client";
+import { useState, useEffect } from "react";
+import { useRouter } from "next/navigation"; // Import useRouter for navigation
+import DashboardHeader from "./dashobardheader";
+import Sidenav from "./sidebar";
+import { GlobalState } from "../../Context/page";
+import StoreProvider from "../../Context/storeProvider";
+import GlobalErrorHandler from "../shared/components/GlobalErrorHandler";
+import AutoTranslator from "./gt";
+import TranslationPanel from "./testtrnslator";
+import { usePathname } from "next/navigation";
+
 export default function DashboardLayout({ children }) {
   const { open } = GlobalState();
-  const [defaultLanguage, setDefaultLanguage] = useState('ja');
+  const [defaultLanguage, setDefaultLanguage] = useState("ja");
   const router = useRouter(); // Initialize router
   const [devToolsOpen, setDevToolsOpen] = useState(false);
 
@@ -19,19 +21,20 @@ export default function DashboardLayout({ children }) {
     const threshold = 160; // Adjust if necessary to detect smaller changes
     const detectDevTools = () => {
       const widthThreshold = window.outerWidth - window.innerWidth > threshold;
-      const heightThreshold = window.outerHeight - window.innerHeight > threshold;
+      const heightThreshold =
+        window.outerHeight - window.innerHeight > threshold;
 
       if (widthThreshold || heightThreshold) {
         if (!devToolsOpen) {
           setDevToolsOpen(true);
-          console.warn('DevTools is open!');
+          console.warn("DevTools is open!");
           // Optional: Log out user or redirect
           // router.push('/unauthorized'); // Redirect user if necessary
         }
       } else {
         if (devToolsOpen) {
           setDevToolsOpen(false);
-          console.log('DevTools is closed.');
+          console.log("DevTools is closed.");
         }
       }
     };
@@ -49,72 +52,136 @@ export default function DashboardLayout({ children }) {
 
   //   const interval = setInterval(clearNetworkLogs, 5000);
 
-  //   return () => clearInterval(interval); 
+  //   return () => clearInterval(interval);
   // }, []);
 
   useEffect(() => {
     // Function to block right-click on images
     const disableRightClick = (event) => {
-      if (event.target.tagName === 'IMG') {
+      if (event.target.tagName === "IMG") {
         event.preventDefault();
       }
     };
 
     // Add event listener to the document
-    document.addEventListener('contextmenu', disableRightClick);
+    document.addEventListener("contextmenu", disableRightClick);
 
     // Cleanup the event listener when the component unmounts
     return () => {
-      document.removeEventListener('contextmenu', disableRightClick);
+      document.removeEventListener("contextmenu", disableRightClick);
     };
   }, []);
 
- 
-//  useEffect(() => {
-//     window.googleTranslateElementInit = () => {
-//       if (!window.googleTranslateInitialized) {
-//         new window.google.translate.TranslateElement(
-//           {
-//             pageLanguage: 'en',
-//             includedLanguages: 'en,es,fr,de,ja',  // Supported languages
-//             layout: window.google.translate.TranslateElement.InlineLayout.SIMPLE,
-//           },
-//           'google_translate_element'
-//         );
-//         window.googleTranslateInitialized = true; // Prevent reinitialization
- 
-//         // Change the language names
-//         const translateElement = document.querySelector('.goog-te-combo');
-//         if (translateElement) {
-//           const options = translateElement.options;
-         
-//           // Example of renaming the language names
-//           for (let i = 0; i < options.length; i++) {
-//             switch (options[i].value) {
-//               case 'en':
-//                 options[i].text = 'Cosmic English'; // Rename "English" to "Cosmic English"
-//                 break;
-//               case 'es':
-//                 options[i].text = 'Cosmic Spanish'; // Rename "Spanish" to "Cosmic Spanish"
-//                 break;
-//               case 'fr':
-//                 options[i].text = 'Cosmic French'; // Rename "French" to "Cosmic French"
-//                 break;
-//               // Add more cases as needed
-//             }
-//           }
-//         }
-//       }
-//     };
- 
-//     if (!document.querySelector('#google-translate-script')) {
-//       const script = document.createElement('script');
-//       script.id = 'google-translate-script';
-//       script.src = 'https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit';
-//       script.async = true;
-//       document.body.appendChild(script);
-//     }
-//   }, []);
+  //  useEffect(() => {
+  //     window.googleTranslateElementInit = () => {
+  //       if (!window.googleTranslateInitialized) {
+  //         new window.google.translate.TranslateElement(
+  //           {
+  //             pageLanguage: 'en',
+  //             includedLanguages: 'en,es,fr,de,ja',  // Supported languages
+  //             layout: window.google.translate.TranslateElement.InlineLayout.SIMPLE,
+  //           },
+  //           'google_translate_element'
+  //         );
+  //         window.googleTranslateInitialized = true; // Prevent reinitialization
+
+  //         // Change the language names
+  //         const translateElement = document.querySelector('.goog-te-combo');
+  //         if (translateElement) {
+  //           const options = translateElement.options;
+
+  //           // Example of renaming the language names
+  //           for (let i = 0; i < options.length; i++) {
+  //             switch (options[i].value) {
+  //               case 'en':
+  //                 options[i].text = 'Cosmic English'; // Rename "English" to "Cosmic English"
+  //                 break;
+  //               case 'es':
+  //                 options[i].text = 'Cosmic Spanish'; // Rename "Spanish" to "Cosmic Spanish"
+  //                 break;
+  //               case 'fr':
+  //                 options[i].text = 'Cosmic French'; // Rename "French" to "Cosmic French"
+  //                 break;
+  //               // Add more cases as needed
+  //             }
+  //           }
+  //         }
+  //       }
+  //     };
+
+  //     if (!document.querySelector('#google-translate-script')) {
+  //       const script = document.createElement('script');
+  //       script.id = 'google-translate-script';
+  //       script.src = 'https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit';
+  //       script.async = true;
+  //       document.body.appendChild(script);
+  //     }
+  //   }, []);
+
+  // useEffect(() => {
+  //   // Dynamically load the Weglot script
+  //   const script = document.createElement("script");
+  //   script.src = "https://cdn.weglot.com/weglot.min.js";
+  //   script.async = true;
+  //   script.onload = () => {
+  //     if (typeof Weglot !== "undefined") {
+  //       Weglot.initialize({
+  //         api_key: "wg_f11f2d476464c018577611c98ed109fa5",
+  //       });
+  //     }
+  //   };
+  //   document.body.appendChild(script);
+
+  //   // Cleanup to remove the script when the component unmounts
+  //   return () => {
+  //     document.body.removeChild(script);
+  //   };
+  // }, []);
+
+  const pathname = usePathname();
+
+  useEffect(() => {
+    const initializeWeglot = () => {
+      if (typeof Weglot !== "undefined") {
+        Weglot.initialize({
+          api_key: "wg_f11f2d476464c018577611c98ed109fa5",
+        });
+      }
+    };
+
+    const reloadWeglot = () => {
+      if (typeof Weglot !== "undefined") {
+        // Force Weglot to reload or re-translate the page content
+        console.log("reloading weglot", Weglot);
+        Weglot.getCache();
+
+        Weglot.translate();
+      }
+    };
+
+    const loadWeglotScript = () => {
+      if (typeof window !== "undefined") {
+        const existingScript = document.getElementById("weglot-script");
+
+        if (!existingScript) {
+          const script = document.createElement("script");
+          script.id = "weglot-script";
+          script.src = "https://cdn.weglot.com/weglot.min.js";
+          script.async = true;
+          script.onload = initializeWeglot;
+          document.body.appendChild(script);
+        } else {
+          initializeWeglot();
+        }
+      }
+    };
+
+    // Load Weglot script on first render
+    loadWeglotScript();
+
+    // Reload Weglot on route changes
+    reloadWeglot();
+  }, [pathname]);
 
   return (
     <>
@@ -129,8 +196,8 @@ export default function DashboardLayout({ children }) {
               <div className="mb-5">
                 <DashboardHeader />
                 {/* <TranslationPanel/> */}
-            {/* <AutoTranslator/> */}
-            {/* <div id="google_translate_element"></div> */}
+                {/* <AutoTranslator/> */}
+                {/* <div id="google_translate_element"></div> */}
                 <div>{children}</div>
               </div>
             </div>
