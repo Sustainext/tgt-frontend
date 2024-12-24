3700c3dfc6cbc645711ec26f2ef26e3f0b784911
Himanshu Banswal <himanshu.banswal@sustainext.ai>
2024-12-24 18:35:03 +0530

    Update pm2.config.js

--- pm2.config.js
+++ pm2.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   apps: [
     {
-      name: "nextjs-app", // Name of the application
-      script: "node_modules/next/dist/bin/next", // Path to the Next.js start script
-      args: "start -p 3000", // Arguments for the start script
+      name: 'sustainext_v2_staging_main',           // Name of the application
+      script: 'node_modules/next/dist/bin/next', // Path to the Next.js start script
+      args: 'start -p 3011',        // Arguments for the start script
       env: {
-        NODE_ENV: "production", // Set the environment to production
+        NODE_ENV: 'production',     // Set the environment to production
       },
     },
   ],
