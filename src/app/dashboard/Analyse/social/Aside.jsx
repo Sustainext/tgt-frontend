563ff7ef5fca6b99f032f30ac586b33e01f44a91
mahendras1ngh <iammahendrasingh@hotmail.com>
2024-12-24 14:40:26 +0530

    config

--- src/app/dashboard/Analyse/social/Aside.jsx
+++ src/app/dashboard/Analyse/social/Aside.jsx
@@ -2,7 +2,9 @@ import React from "react";
 
 const Aside = ({ activeTab, handleTabClick }) => {
   return (
-    <div className={`m-3 ml-2 p-2 border border-r-2 border-b-2 shadow-lg rounded-lg sticky top-[5rem]`}>
+    <div
+      className={`m-3 ml-2 p-2 border border-r-2 border-b-2 shadow-lg rounded-lg sticky top-[5rem]`}
+    >
       <div className="flex items-start py-4 min-w-[200px] min-h-[100vh] rounded-lg text-[0.875rem]">
         <div className="flex flex-col w-full font-medium">
           <div className="flex-col justify-end items-start gap-2 inline-flex mb-8">
@@ -21,7 +23,6 @@ const Aside = ({ activeTab, handleTabClick }) => {
             }`}
             onClick={() => handleTabClick("Tab1")}
           >
-        
             <span className="mr-7 text-left">Employment</span>
           </button>
           <div>
@@ -33,12 +34,13 @@ const Aside = ({ activeTab, handleTabClick }) => {
               }`}
               onClick={() => handleTabClick("Tab2")}
             >
-         
-              <span className="mr-12 text-left">Occupational health and safety</span>
+              <span className="mr-12 text-left">
+                Occupational health and safety
+              </span>
               <div className="inset-y-0 -right-2 flex items-center pointer-events-none"></div>
             </button>
           </div>
-          {/* <button
+          <button
             className={`flex items-center justify-start px-2 py-2 mb-1 focus:outline-none w-full ${
               activeTab === "Tab7"
                 ? "text-[#007EEF] "
@@ -46,9 +48,8 @@ const Aside = ({ activeTab, handleTabClick }) => {
             }`}
             onClick={() => handleTabClick("Tab7")}
           >
-
             <span className="mr-8 text-left">Training</span>
-          </button> */}
+          </button>
           <button
             className={`flex items-center justify-start px-2 py-2 mb-1 focus:outline-none w-full ${
               activeTab === "Tab5"
@@ -57,7 +58,6 @@ const Aside = ({ activeTab, handleTabClick }) => {
             }`}
             onClick={() => handleTabClick("Tab5")}
           >
-
             <span className="mr-8 text-left">Diversity & Inclusion</span>
           </button>
           <button
@@ -68,10 +68,9 @@ const Aside = ({ activeTab, handleTabClick }) => {
             }`}
             onClick={() => handleTabClick("Tab8")}
           >
-
             <span className="mr-8 text-left">Non-Discrimination</span>
           </button>
-          
+
           <button
             className={`flex items-center justify-start px-2 py-2 mb-1 focus:outline-none w-full ${
               activeTab === "Tab9"
@@ -80,7 +79,6 @@ const Aside = ({ activeTab, handleTabClick }) => {
             }`}
             onClick={() => handleTabClick("Tab9")}
           >
-
             <span className="mr-8 text-left">Collective Bargaining</span>
           </button>
           <button
@@ -91,10 +89,9 @@ const Aside = ({ activeTab, handleTabClick }) => {
             }`}
             onClick={() => handleTabClick("Tab3")}
           >
-            
             <span className="mr-12 text-left">Child Labour</span>
-          </button> 
-        <button
+          </button>
+          <button
             className={`flex items-center justify-start px-2 py-2 mb-1 focus:outline-none w-full ${
               activeTab === "Tab4"
                 ? "text-[#007EEF] "
@@ -102,10 +99,9 @@ const Aside = ({ activeTab, handleTabClick }) => {
             }`}
             onClick={() => handleTabClick("Tab4")}
           >
-
             <span className="mr-8 text-left">Forced or Compulsory Labour</span>
           </button>
-     
+
           <button
             className={`flex items-center justify-start px-2 py-2 mb-1 focus:outline-none w-full ${
               activeTab === "Tab15"
@@ -114,7 +110,6 @@ const Aside = ({ activeTab, handleTabClick }) => {
             }`}
             onClick={() => handleTabClick("Tab15")}
           >
-
             <span className="mr-8 text-left">Security Personnel</span>
           </button>
 
@@ -126,7 +121,6 @@ const Aside = ({ activeTab, handleTabClick }) => {
             }`}
             onClick={() => handleTabClick("Tab10")}
           >
-
             <span className="mr-8 text-left">Community Development</span>
           </button>
           <button
