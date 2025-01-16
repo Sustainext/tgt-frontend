95c77d85036183aa08bfa67acb126327779d5af0
Himanshu Banswal <himanshu.banswal@sustainext.ai>
2025-01-16 16:32:17 +0530

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,25 +1,17 @@
 const { watch } = require("fs");
-
 module.exports = {
   apps: [
     {
-      name: "sustainextv2_staging_main",
-
-      script: "npm",
-
-      args: "run start",
-
+      name: 'sustainextv2',
+      script: 'npm',
+      args: 'run start',
       // watch: false,
-
       // exec_mode: 'fork', // optional, defaults to 'fork'
-
-      interpreter: "none", // use the system's default interpreter
-
+      interpreter: 'none', // use the system's default interpreter
       env: {
-        NODE_ENV: "production",
-
-        PORT: 3011, // you can set the port here
-      },
+        NODE_ENV: 'production',
+        PORT: 3001 // you can set the port here
+      }
     },
   ],
 };
