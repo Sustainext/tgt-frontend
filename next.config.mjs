0afa468e71f60c3d938774036665952b959a2bd8
mahendras1ngh <iammahendrasingh@hotmail.com>
2025-02-14 16:08:47 +0530

    next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 const nextConfig = {
   env: {
-    BACKEND_API_URL: "https://udm-be.sustainext.ai",
+    BACKEND_API_URL: "https://udm-be-dev.sustainext.ai",
     // BACKEND_API_URL: 'http://127.0.0.1:8000',
     NEXT_APP_ZOHO_URL_EMISSIONS:
       "https://analytics.zoho.in/open-view/283231000002209106",
@@ -13,14 +13,14 @@ const nextConfig = {
 
     // SSO
     AUTH0_SECRET: "LONG RANDOM VALsUE",
-    // AUTH0_BASE_URL: "https://sustainext-udm.sustainext.ai",
-    AUTH0_BASE_URL: "http://localhost:3000",
+    AUTH0_BASE_URL: "https://sustainext-udm.sustainext.ai",
+    // AUTH0_BASE_URL: "http://localhost:3000",
     AUTH0_ISSUER_BASE_URL: "https://dev-0biozzwskqs6o65f.us.auth0.com",
     AUTH0_CLIENT_ID: "Jr9m3MSh0oJr6h2a876tPN5h44OU4Bo8",
     AUTH0_CLIENT_SECRET:
       "VcLShjMepGqKoT5t9MwsuyGPHbWwwEN6zBl62XsXPSlkdHlVXeZFeRjpi6qX7t-q",
-    // AUTH0_REDIRECT_URL: "https://sustainext-udm.sustainext.ai/callback",
-    AUTH0_REDIRECT_URL: "http://localhost:3000/callback",
+    AUTH0_REDIRECT_URL: "https://sustainext-udm.sustainext.ai/callback",
+    // AUTH0_REDIRECT_URL: "http://localhost:3000/callback",
   },
  
   images: {
