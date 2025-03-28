2ff4e080dbf6b9a7677be2815d193bc055fd11b4
himanshu-next <himanshu.banswal@sustainext.ai>
2025-03-28 09:54:13 +0530

    redis configuration

--- src/lib/redis.js
+++ src/lib/redis.js
@@ -1,18 +1,18 @@
-import Redis from 'ioredis';
+// import Redis from 'ioredis';
 
-// const redis = new Redis({
-//   host: process.env.REDIS_HOST,
-//   port: Number(process.env.REDIS_PORT),
-//   password: process.env.REDIS_PASSWORD,
-//   tls: process.env.REDIS_TLS === 'true' ? {} : undefined,
-// });
+// // const redis = new Redis({
+// //   host: process.env.REDIS_HOST,
+// //   port: Number(process.env.REDIS_PORT),
+// //   password: process.env.REDIS_PASSWORD,
+// //   tls: process.env.REDIS_TLS === 'true' ? {} : undefined,
+// // });
 
-const redis = new Redis({
-  host: '127.0.0.1',
-  port: 6379,
-  // password: 'YOUR_REDIS_PASSWORD', // Only if you set one
-  // Optional: add TLS if you're securing Redis
-  // tls: {}
-});
+// // const redis = new Redis({
+// //   host: '127.0.0.1',
+// //   port: 6379,
+// //   // password: 'YOUR_REDIS_PASSWORD', // Only if you set one
+// //   // Optional: add TLS if you're securing Redis
+// //   // tls: {}
+// // });
 
-export default redis;
+// export default redis;
