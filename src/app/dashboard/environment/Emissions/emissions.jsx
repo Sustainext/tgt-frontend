ed0ad2779052d30b974e7fb1199094077c65e147
shubham <152493900+shubham-sustainext@users.noreply.github.com>
2025-06-12 09:49:22 +0530

    Update emissions.jsx

--- src/app/dashboard/environment/Emissions/emissions.jsx
+++ src/app/dashboard/environment/Emissions/emissions.jsx
@@ -54,7 +54,7 @@ const Emissions = ({ open, apiData, setMobileopen }) => {
     setData(newData);
   }, [category]);
 
-  const showToast = (header, body, gradient, duration = 50000) => {
+  const showToast = (header, body, gradient, duration = 5000) => {
     const id = Date.now();
     setToastQueue((prev) => [...prev, { id, header, body, gradient }]);
 
